ec72008f4241859611bc0ee5fa00db4fcbf87a53
Mileto Di Marco <mileto.di.marco@howest.be>
2021-03-18 11:33:21 +0100

    add data annotations

--- Wba.FormViewModelExample.Web/ViewModels/AccountLoginViewModel.cs
+++ Wba.FormViewModelExample.Web/ViewModels/AccountLoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,13 @@ namespace Wba.FormViewModelExample.Web.ViewModels
 {
     public class AccountLoginViewModel
     {
+        [Display(Name = "Email:")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Paswoord:")]
         public string Password { get; set; }
+        [Display(Name = "Onthoud mij")]
         public bool RememberMe { get; set; }
     }
 }
--- Wba.FormViewModelExample.Web/ViewModels/UsersRegisterViewModel.cs
+++ Wba.FormViewModelExample.Web/ViewModels/UsersRegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,18 @@ namespace Wba.FormViewModelExample.Web.ViewModels
 {
     public class UsersRegisterViewModel
     {
+        [Display(Name ="Email:")]
+        [DataType(DataType.EmailAddress)]
         public string UserName { get; set; }
+        [Display(Name = "Voornaam:")]
         public string Firstname { get; set; }
+        [Display(Name = "Naam:")]
         public string Lastname { get; set; }
+        [Display(Name = "Geef een paswoord in:")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Herhaal uw paswoord:")]
         public string RepeatPassword { get; set; }
     }
 }
