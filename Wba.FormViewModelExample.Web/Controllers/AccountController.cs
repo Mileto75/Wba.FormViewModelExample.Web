b7a3b710b7507036cb95ec2683874144f8432ced
Mileto Di Marco <mileto.di.marco@howest.be>
2021-03-18 11:10:06 +0100

    Add project files.

---
+++ Wba.FormViewModelExample.Web/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wba.FormViewModelExample.Web.ViewModels;
+
+namespace Wba.FormViewModelExample.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            //shows the form
+            //make a form viewModel
+            AccountLoginViewModel accountLoginViewModel = new AccountLoginViewModel();
+            //set remember me
+            accountLoginViewModel.RememberMe = true;
+            return View(accountLoginViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Login(AccountLoginViewModel accountLoginViewModel)
+        {
+            //gets the form data
+            string Email = accountLoginViewModel?.Email;
+            return Content(Email);
+        }
+    }
+}
---
+++ Wba.FormViewModelExample.Web/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wba.FormViewModelExample.Web.ViewModels;
+
+namespace Wba.FormViewModelExample.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register
+            (UsersRegisterViewModel usersRegisterViewModel)
+        {
+            //perform validation
+            //store user in Db
+            //redirect to ...
+            return RedirectToAction("RegistrationSuccess");
+        }
+
+        [HttpGet]
+        [Route("thanks-for-registering")]
+        public IActionResult RegistrationSuccess
+            ()
+        {
+            return View();
+        }
+
+
+    }
+}
